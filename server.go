5699bfda31e257b747c77e76960b2f55d0b18c96
eRaBxEs <ehiojee@yahoo.com>
2022-09-21 09:26:01 +0100

    set up gin for project

--- server.go
+++ server.go
@@ -3,28 +3,68 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
+	"time"
 	"todo-gin-graphql/graph"
 	"todo-gin-graphql/graph/generated"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-const defaultPort = "7070"
+const defaultPort = ":7070"
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	h := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver()}))
+	// Configure WebSocket with CORS
+	h.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+		KeepAlivePingInterval: 10 * time.Second,
+	})
+
+	h.AddTransport(transport.Options{})
+	h.AddTransport(transport.GET{})
+	h.AddTransport(transport.POST{})
+	h.AddTransport(transport.MultipartForm{})
+
+	h.SetQueryCache(lru.New(1000))
+
+	h.Use(extension.Introspection{})
+	h.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL playground", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	//srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver()}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// Setting up Gin
+	r := gin.Default()
+	r.Any("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	log.Printf("connect to http://localhost%s/ for GraphQL playground", defaultPort)
+	r.Run(defaultPort)
 }
